0fc3c64d24b0a61e9a46a81cd71cea565bbdd9bf
Sam Joseph <tansaku@gmail.com>
2020-01-17 10:52:13 +0000

    first pass connecting to postcodes.io - initial acceptance tests passing

--- app/controllers/postcode_lookup_controller.rb
+++ app/controllers/postcode_lookup_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
+require "postcodes_io"
+
 class PostcodeLookupController < ApplicationController
   def index
     @postcode = params[:postcode]
+    pio = Postcodes::IO.new
+    postcode = pio.lookup(@postcode) if @postcode
+    @whitelisted = postcode.lsoa.start_with?(/Southwark|Lambeth/) if postcode
   end
 end
